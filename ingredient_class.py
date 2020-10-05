7644bc81b21ccaf8ec112a5cec28012d2bef63ba
Sara Kaminska <sara.kaminska@hotmail.com>
2020-10-05 15:33:01 +0200

    Added a print method to the ingredient class

--- ingredient_class.py
+++ ingredient_class.py
@@ -3,16 +3,25 @@ class Ingredient:
         self.sort = sort
         self.amount = amount
 
+    def print_ingredient(self):
+        print(f"{self.sort} ({self.amount} g)")
+
 
 class Hops(Ingredient):
     def __init__(self, sort, amount, boilingtime):
-        super().__init__()
+        super().__init__(sort, amount)
         self.boilingtime = boilingtime
+
+    def print_ingredient(self):
+        print(f"{self.sort} ({self.amount} g). Koktid: {self.boilingtime} minuter")
 
 
 def main():
-    pass
+    malt = Ingredient("Pale Ale", 2500)
+    humle = Hops("Citra", 10, 60)
 
+    malt.print_ingredient()
+    humle.print_ingredient()
 
 if __name__ == "__main__":
     main()
